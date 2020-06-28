f37053f3ff2710c6ac4b897f4abc8ec639fae190
evaGDLT <evagarciadelatorre@MBP-de-Eva.home>
2020-06-28 18:42:54 +0200

    added responsibility design

--- js/main.js
+++ js/main.js
@@ -50,75 +50,10 @@ function unhoverTecnologies(element, tecnology) {
 
 }
 
-// const tecnologiesHover = () => {
-//     const tecnology1 = document.querySelectorAll('.tecnology');
-//     // const project2 = document.querySelectorAll('.project')[1];
-//     // const project3 = document.querySelectorAll('.project')[2];
-//     // const project4 = document.querySelectorAll('.project')[3];
+function hoverProject(element, tipoRepo) {
 
-//     const title1 = document.querySelector('.title1');
-//     // const title2 = document.querySelector('.title2');
-//     // const title3 = document.querySelector('.title3');
-//     // const title4 = document.querySelector('.title4');
+}
 
-//     title1.classList.add('titleHide');
-//     // title2.classList.add('titleHide')
-//     // title3.classList.add('titleHide')
-//     // title4.classList.add('titleHide')
-
-//     tecnology1.addEventListener("mouseover", event => {
-//         if (title1.classList.contains('titleHide')) {
-//             title1.classList.remove('titleHide')
-//             title1.classList.add('titleShow')
-//         }
-//     });
-
-//     tecnology1.addEventListener("mouseout", event => {
-//         if (title1.classList.contains('titleShow')) {
-//             title1.classList.remove('titleShow')
-//             title1.classList.add('titleHide')
-//         }
-//     });
-
-//     // project2.addEventListener("mouseover", event => {
-//     //     if (title2.classList.contains('titleHide')) {
-//     //         title2.classList.remove('titleHide')
-//     //         title2.classList.add('titleShow')
-//     //     }
-//     // });
-
-//     // project2.addEventListener("mouseout", event => {
-//     //     if (title2.classList.contains('titleShow')) {
-//     //         title2.classList.remove('titleShow')
-//     //         title2.classList.add('titleHide')
-//     //     }
-//     // });
-
-//     // project3.addEventListener("mouseover", event => {
-//     //     if (title3.classList.contains('titleHide')) {
-//     //         title3.classList.remove('titleHide')
-//     //         title3.classList.add('titleShow')
-//     //     }
-//     // });
-
-//     // project3.addEventListener("mouseout", event => {
-//     //     if (title3.classList.contains('titleShow')) {
-//     //         title3.classList.remove('titleShow')
-//     //         title3.classList.add('titleHide')
-//     //     }
-//     // });
-
-//     // project4.addEventListener("mouseover", event => {
-//     //     if (title4.classList.contains('titleHide')) {
-//     //         title4.classList.remove('titleHide')
-//     //         title4.classList.add('titleShow')
-//     //     }
-//     // });
-
-//     // project4.addEventListener("mouseout", event => {
-//     //     if (title4.classList.contains('titleShow')) {
-//     //         title4.classList.remove('titleShow')
-//     //         title4.classList.add('titleHide')
-//     //     }
-//     // });
-// }
+function unhoverProject(element) {
+    element.setAttribute('src', `./imgs/portfolio.png`);
+}
